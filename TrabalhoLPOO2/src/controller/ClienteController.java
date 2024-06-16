4df3d64862be1e718d41c49cdd6cf76709fe08ca
JanainaNogueira <janainanogueira464@gmail.com>
2024-06-16 00:08:00 -0300

    feat: Criada clientecontroller

--- TrabalhoLPOO2/src/controller/ClienteController.java
+++ TrabalhoLPOO2/src/controller/ClienteController.java
@@ -8,6 +8,70 @@ package controller;
  *
  * @author janai
  */
+
+import model.Cliente;
+import telas.Tela1;
+import dao.ClienteDaoSql;
+
+
 public class ClienteController {
+    private Tela1 view; 
+    private ClienteDaoSql clienteDAO;
     
+    public ClienteController(Tela1 view, ClienteDaoSql clienteDAO){
+        this.view = view;
+        this.clienteDAO = clienteDAO;
+        initController();
+    }
+    
+    private void initController(){
+        this.view.setControllerCliente(this);
+        this.view.initView();
+    }
+    
+    public void createCliente(){
+        try{
+            Cliente cliente = view.getClienteFormulario();
+            clienteDAO.insert(cliente);
+            view.inserirClienteView(cliente);
+            view.apresentaInfo("Cliente criado");
+        }catch(Exception ex){
+            view.apresentaErro("Erro ao criar cliente");
+        }
+    }
+    
+    public void updateCliente(){
+        try{
+            Cliente cliente = view.getClienteUpdate();
+            if(cliente==null){
+                view.apresentaInfo("Selecione um cliente para atualizar");
+                return;
+            }
+            clienteDAO.update(cliente);
+            view.updateCliente(cliente);
+        }catch(Exception ex){
+            view.apresentaErro("Erro ao apresentar cliente");
+        }
+    }
+    
+    public void showCliente(){
+        try{
+            view.clearUpdateCliente();
+            List<Cliente> lista= this.clienteDAO.getAll();
+            view.showListCliente(lista);
+        }catch(Exception ex){
+            ex.printStackTrace();
+            view.apresentaErro(erro);
+        }
+    }
+    public void deleteCliente(){
+        try{
+            List<Cliente> listDelete = view.getClienteDelete();
+            clienteDAO.delete(listDelete);
+            view.deleteClienteView(listDelete);
+        }catch(Exception ex){
+            view.apresentaErro("Erro ao excluir o cliente");
+        }
+    }
 }
+
